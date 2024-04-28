0d456b159af3739236f5594620a06db426b868c7
eyosibel <eyosiyasbelete63@gmail.com>
2024-04-28 11:30:00 +0300

    last part

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,11 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-//import { app, server } from "./socket/socket.js";
-const app = express();
+import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
-//const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -24,13 +23,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-//app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-//app.get("*", (req, res) => {
-	//res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-//});
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
 });
