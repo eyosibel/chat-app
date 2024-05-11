dcfa2d5b870636ad233a8bd492771e94fef77ca8
eyosibel <eyosiyasbelete63@gmail.com>
2024-05-11 21:39:37 +0300

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-production.onrender.com", {
+			const socket = io("https://chat-app-production-3gng.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
